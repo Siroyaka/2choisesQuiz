967dc5f79e17f130601e6d62c465cf5252e97d1a
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-09-09 22:43:58 +0900

    ios用の暫定対応

--- src/components/Template/QuizTemplate.tsx
+++ src/components/Template/QuizTemplate.tsx
@@ -34,17 +34,44 @@ const QuizTemplate: React.FC<Props> = (props) => {
     dispatch({
       type: QuizResultTypes.INITIALIZE
     });
-    // AudioContextのinitializeはここでやる
-    // callbackでロード完了を通知する
-    se.current = new SoundEffect();
-    se.current.pushSource('/asset/collect_sound.wav', 'collect_sound');
-    se.current.pushSource('/asset/wrong_sound.wav', 'wrong_sound');
-    se.current.pushSource('/asset/tick_sound.wav', 'tick_sound');
-    se.current.onload = () => setEndInitialize(true);
-    se.current.load();
   }, []);
+
+  const onClickStart = (flg: boolean) => {
+    if(flg) {
+      // AudioContextのinitializeはここでやる
+      // callbackでロード完了を通知する
+      se.current = new SoundEffect();
+      se.current.pushSource('/asset/collect_sound.wav', 'collect_sound');
+      se.current.pushSource('/asset/wrong_sound.wav', 'wrong_sound');
+      se.current.pushSource('/asset/tick_sound.wav', 'tick_sound');
+      se.current.onload = () => setEndInitialize(true);
+      se.current.load();
+      return;
+    }
+    se.current = null;
+    setEndInitialize(true);
+  }
   return(
     <React.Fragment>
+      {!endInitialize && 
+      <div className='flex flex-col items-center'>
+        <span className='text-3xl'>iPhone、iPadをお使いの人は音なしを選択してください。</span>
+        <div className='flex flex-row pt-2 items-center justify-center'>
+          <button
+            className='border bg-blue-300 rounded-full py-2 px-4 mx-4 text-2xl focus:outline-none '
+            onClick={() => onClickStart(true)}
+          >
+            音あり
+          </button>
+          <button
+            className='border bg-blue-300 rounded-full py-2 px-4 mx-4 text-2xl focus:outline-none '
+            onClick={() => onClickStart(false)}
+          >
+            音なし
+          </button>
+        </div>
+      </div>
+      }
       {!result.isFinished && endInitialize &&
         <QuizForm
           {...props}
