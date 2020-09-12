79a3b057dcbea72a0041115977d46e43682325a7
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-09-12 19:33:19 +0900

    トップページのHead要素作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 
 import Link from 'next/link';
+import Head from 'next/head';
 
 import PagesData from 'lib/PagesData';
 
+const HeadItems: React.FC = () => {
+  return(
+    <Head>
+      <title>2択クイズ</title>
+    </Head>
+  )
+}
+
 const Home: React.FC = () => {
   return (
-    <div className='my-2 mx-4'>
-      {PagesData.map(x => (
-        <nav key={'contents-' + x.title}>
-          <Link href={x.href}>
-            <button className='p-4 border hover:bg-blue-300 bg-blue-400 rounded-full'>
-              {x.title}
-            </button>
-          </Link>
-        </nav>
-      ))}
-    </div>
+    <React.Fragment>
+      <HeadItems />
+      <div className='my-2 mx-4'>
+        {PagesData.map(x => (
+          <nav key={'contents-' + x.title}>
+            <Link href={x.href}>
+              <button className='p-4 border hover:bg-blue-300 bg-blue-400 rounded-full'>
+                {x.title}
+              </button>
+            </Link>
+          </nav>
+        ))}
+      </div>
+    </React.Fragment>
   )
 }
 
