385e3bdd0c772266ac123da3a4d76503f498f42a
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-09-22 20:07:06 +0900

    調整

--- src/pages/choise_quiz/multi1.tsx
+++ src/pages/choise_quiz/multi1.tsx
@@ -21,13 +21,13 @@ const HeadItem: React.FC<{title: string}> = (props) => {
       <title>{title}</title>
       <meta key='ogUrl' property="og:url" content={`${InitialMetaData.ogUrl}/choise_quiz/multi1`}/>
       <meta key='ogTitle' property="og:title" content={title}/>
-      <meta key='ogDescription' property="og:description" content={'1桁の掛け算を10問解いてみよう！'}/>
+      <meta key='ogDescription' property="og:description" content={'1桁の掛け算を20問解いてみよう！4択の中から正解を選んでね！'}/>
     </Head>
   )
 }
 
 const QuizPage: React.FC<Props> = (props) => {
-  const title = '掛け算10問クイズ';
+  const title = '掛け算4択クイズ';
   return(
     <React.Fragment>
       <HeadItem title={title}/>
@@ -35,7 +35,7 @@ const QuizPage: React.FC<Props> = (props) => {
         <QuizComponent
           title={title}
           hashTags={[...commonHashTags, title]}
-          quiz={make1MulQuiz(3)}
+          quiz={make1MulQuiz(4)}
           quizLength={10}
           startCountdown={4}
         />
