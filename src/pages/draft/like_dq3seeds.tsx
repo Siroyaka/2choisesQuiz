a534e8e77edb36b7b598c4a4aa3fa415b388b06a
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-09-29 14:46:51 +0900

    不要なものを削除

--- src/pages/draft/like_dq3seeds.tsx
+++ src/pages/draft/like_dq3seeds.tsx
@@ -32,9 +32,6 @@ const LikeDqSeedsPage: React.FC<Props> = (props) => {
         <QuestionComponent
           title='仲間作成'
           quiz={makeTestAccumulations}
-          captionSpeed={10}
-          timeLimit={10}
-          countdownSpeed={1000}
           valueNames={['体力', '精神', '攻撃力', '防御力', '運']}
           initialValues={[10, 20, 5, 5, 3]}
           doExit={exitOfQuestionCount(5)}
